b4215e89e4324ff4d63a01db50aef87c8bdae6f6
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-11-16 00:47:19 +0100

    01/added bash script

---
+++ level01/do_john.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+wget https://download.openwall.net/pub/projects/john/contrib/macosx/john-1.8.0.9-jumbo-macosx_sse4.zip
+
+tar -xvf john-1.8.0.9-jumbo-macosx_sse4.zip
+
+cd john-1.8.0.9-jumbo-macosx_sse4/run
+
+echo "42hDRfypTqqnw" > __test
+./john __test
+./john --show __test
+
+cd ../..
+
+rm -rf john*
+
