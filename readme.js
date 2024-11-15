9bf4169a2521efd6f8e7a730ba79bb91b71a061c
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2024-11-15 23:55:50 +0100

    Create readme.js

---
+++ readme.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const path = require('path')
+
+function get_readme_path(dir) {
+  let res = []  
+  const files = fs.readdirSync(dir)
+  files.forEach(file => {
+    const filepath = path.join(dir, file)
+    const stats = fs.statSync(filepath)
+    if (stats.isDirectory() && file.startsWith('level')) {
+      res.push(get_readme_path(filepath))
+    } else if (stats.isFile() && file === 'README.mdx') {
+      res.push(filepath)
+    }
+  })
+  return res
+}
+
+function go() {
+  let content = ''
+  const readmes = get_readme_path(__dirname)
+
+  readmes.forEach(path => {
+    const content = '\n\n' + fs.readFileSync(path, 'utf-8')
+    // content = '\n\n' + content
+  })
+
+  return content
+}
+
+const README = go()
+console.log(README)
