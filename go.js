53138c29a7d3c2f98a28d7908b3bd57b089c6e0b
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2024-11-16 17:10:29 +0100

    Update go.js

--- go.js
+++ go.js
@@ -26,7 +26,7 @@ function get_readme_path(dir) {
 
 function get_readme_content() {
 
-  let content = fs.readFileSync(dirname + '/tokens.mdx', 'utf-8')
+  let content = fs.readFileSync(__dirname + '/tokens.mdx', 'utf-8')
   const readmes = get_readme_path(__dirname)
   // console.log(__dirname)
   // /home/runner/work/cfo/cfo
