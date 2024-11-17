7468e9b31c2942a50bb55c1244e237cfce3abed5
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2024-11-17 20:29:13 +0100

    Update decode.py

--- level00/decode.py
+++ level00/decode.py
@@ -1,3 +1,3 @@
-a = ord('a')
+a, s = ord('a'), 'cdiiddwpgswtgt'
 for i in range(26):
-    print(i, ''.join(chr(a + (ord(c) - a + i) % 26) for c in 'cdiiddwpgswtgt'))
+    print(i, ''.join(chr(a + (ord(c) - a + i) % 26) for c in s))
