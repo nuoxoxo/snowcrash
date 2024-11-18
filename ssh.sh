1b8c0b2e160c06d1c4d2ed2f6235c5267ef3d03f
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-11-18 13:45:07 +0100

    ssh/added arg check and sub

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-ssh level09@$(ifconfig | grep 'inet ' | awk 'NR==2 {print $2}') -p 4242
+# old
+#ssh level09@$(ifconfig | grep 'inet ' | awk 'NR==2 {print $2}') -p 4242
 
+if [[ $# -eq 1 && "$1" -ge 0 && "$1" -le 14 ]]; then
+    #lv="level$1"
+    if [[ "$1" -lt 10 ]]; then
+        lv="level0$1"
+    else
+        lv="level$1"
+    fi
+else
+    lv="level00"
+    #echo "~[0, 15)"
+    #exit 1
+fi
+
+ssh ${lv}@$(ifconfig | grep 'inet ' | awk 'NR==2 {print $2}') -p 4242
