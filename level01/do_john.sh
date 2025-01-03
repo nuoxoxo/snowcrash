6004c685d9a832d6d6f2b1d1e240a15f1c710e65
nuoxoxo <malcolm.nuo.xu@gmail.com>
2025-01-03 17:18:36 +0100

    fix/ level 01

--- level01/do_john.sh
+++ level01/do_john.sh
@@ -1,13 +1,24 @@
+: '
 wget https://download.openwall.net/pub/projects/john/contrib/macosx/john-1.8.0.9-jumbo-macosx_sse4.zip
-unzip john-1.8.0.9-jumbo-macosx_sse4.zip
 # tar -xvf john-1.8.0.9-jumbo-macosx_sse4.zip
+unzip john-1.8.0.9-jumbo-macosx_sse4.zip
 cd john-1.8.0.9-jumbo-macosx_sse4/run
-# echo "42hDRfypTqqnw" > infile
+'
+
+if ! [ -d "john" ]; then
+    git clone https://github.com/openwall/john.git
+    cd john/src && ./configure && make
+fi
+
 scp -P 4242 level01@$(ifconfig|grep 'inet '|sort|sed -n '4p'|awk -F' ' '{print $2}'):/etc/passwd $(pwd)/infile
+
 cat infile|grep flag01|awk -F: '{print $2}'
-cat infile|grep flag01|awk -F: '{print $2}' > infile
+cat infile|grep flag01 > infile
 cat infile
-./john < infile
+
+cd john/run
+./john infile
 ./john --show infile
 cd ../..
-rm -rf john*
+
+# rm -rf john*
